ca640d4cb9f3cade18bdcaa7fda71023be62d459
Mrigank Saksena <44930655+mrigank-saksena@users.noreply.github.com>
2020-01-17 19:04:39 -0500

    https redirect

--- config.php
+++ config.php
@@ -1,6 +1,15 @@
 <?php
 
+if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+{
+    //Tell the browser to redirect to the HTTPS URL.
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
+    //Prevent the rest of the script from executing.
+    exit;
+}
+
 //SITE GLOBAL CONFIGURATION
 $email = "mriganksaksena97@gmail.com";   
+
 
 ?>
